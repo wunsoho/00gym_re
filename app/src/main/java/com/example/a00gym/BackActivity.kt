47f9cbea3389330301a3676cd192cafcf7df2aa1
origin <wunsoho@naver.com>
2023-10-09 18:01:54 +0900

    project init

---
+++ app/src/main/java/com/example/a00gym/BackActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a00gym
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_back)
+    }
+}
